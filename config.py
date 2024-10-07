730bdb30d28f1d2925ed650665fd5d0379eb2c90
Jernish <111893985+Jernish-FDO@users.noreply.github.com>
2024-10-08 00:01:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 
 # Token of Telegram bot
-TOKEN = ""
+TOKEN = "7865849668:AAGMXPSgbQf5j2ihbbCSOMLcq6khVU6A7u0"
 
 
 # API key of TMDB (v3 auth)
-API_KEY= ""
+API_KEY= "a7363080a8ef7ff38d6a91fa7538d073"
